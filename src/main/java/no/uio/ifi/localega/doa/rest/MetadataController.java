bc54bcd999d34684aa8b774f6d5b5f753b49fed3
teemu.kataja <teemu.kataja@csc.fi>
2021-02-26 10:41:08 +0200

    validate base64 string

--- src/main/java/no/uio/ifi/localega/doa/rest/MetadataController.java
+++ src/main/java/no/uio/ifi/localega/doa/rest/MetadataController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 import java.util.Base64;
+import java.util.regex.Pattern;
 
 /**
  * REST controller incorporating metadata-related endpoints.
@@ -54,12 +55,19 @@ public class MetadataController {
     @GetMapping("/datasets/{datasetId}/files")
     public ResponseEntity<?> files(@PathVariable(value = "datasetId") String datasetId) {
         Set<String> datasetIds = (Set<String>) request.getAttribute(AAIAspect.DATASETS);
+        Pattern BASE64_PATTERN = Pattern.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$");
         String datasetIdParsed = "";
-        try {
-            // attempt to base64 decode the path param
-            datasetIdParsed = new String(Base64.getDecoder().decode(datasetId.getBytes()));
-        } catch (Exception e) {
-            // path param was not in base64 format
+        // test if the string is valid base64 before attempting to decode it
+        if (BASE64_PATTERN.matcher(datasetId).matches()) {
+            try {
+                // attempt to base64 decode the path param
+                datasetIdParsed = new String(Base64.getDecoder().decode(datasetId.getBytes()));
+            } catch (Exception e) {
+                // decoding failed, fall back to original string
+                datasetIdParsed = datasetId;
+            }
+        } else {
+            // use the original string
             datasetIdParsed = datasetId;
         }
         if (!datasetIds.contains(datasetIdParsed)) {
